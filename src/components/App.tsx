2b7b22a609c055f450839185bebe1b90a757c20d
mahume <michael.a.hume@gmail.com>
2021-03-20 13:18:18 -0400

    edit: App state to support test data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AppHeader from "./AppHeader";
 import { StyledLayout, StyledMain } from "./App.styles";
 import Card from "./Card";
+// import { fetchProduct } from "../utils/api";
+import { enginesTestData, workstationsTestData } from "../utils/testData";
+import { IEngine, IWorkstation } from "../typescript/interfaces/data";
 
 function App() {
+  const [engines, setEngines] = useState<IEngine[]>([]);
+  const [workstations, setWorkstations] = useState<IWorkstation[]>([]);
+
+  useEffect(() => {
+    // TODO: Fetch from API
+
+    setEngines(enginesTestData);
+    setWorkstations(workstationsTestData);
+  }, []);
+
   return (
     <StyledLayout>
       <AppHeader />
       <StyledMain>
-        <Card />
+        {workstations.map((station) => {
+          return <Card />;
+        })}
       </StyledMain>
     </StyledLayout>
   );
