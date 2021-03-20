56d2b468813b4afa2c6d8aecaa4320966f3e75f2
mahume <michael.a.hume@gmail.com>
2021-03-20 11:41:52 -0400

    edit: Create typing for custom theme palette

---
+++ src/styled.d.ts
@@ -0,0 +1,20 @@
+import "styled-components";
+
+interface IPalette {
+  main: string;
+  success: string;
+  contrastText: string;
+}
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    palette: {
+      common: {
+        black: string;
+        white: string;
+        gray: string;
+      };
+      primary: IPalette;
+    };
+  }
+}
--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,15 +1,16 @@
-const theme = {
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
   palette: {
-    color: {
-      primary: "hsl(221, 40%, 11%)",
-      secondary: "hsl(220, 9%, 46%)",
-      contrast: "hsl(99, 100%, 100%)",
+    common: {
+      black: "hsl(221, 40%, 11%)",
+      white: "hsl(99, 100%, 100%)",
+      gray: "hsl(220, 9%, 46%)",
     },
-    backgroundColor: {
-      light: "hsl(99, 100%, 100%)",
-      gray: "hsl(105, 0%, 90%)",
-      dark: "hsl(216, 28%, 17%)",
-      blue: "hsl(225, 64%, 33%)",
+    primary: {
+      main: "hsl(225, 64%, 33%)",
+      success: "hsl(161, 94%, 30%)",
+      contrastText: "hsl(99, 100%, 100%)",
     },
   },
 };
