d0f031ec72a512f7fef08b25fefc77c4468fcce2
mahume <michael.a.hume@gmail.com>
2021-03-22 08:50:27 -0400

    fix: change data from test to fetch

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -1,20 +1,37 @@
 import React, { createContext, useEffect, useState, ReactNode } from "react";
 import { IData } from "../typescript/interfaces/data";
 import { INITIAL_DATA } from "../constants/data";
-import { enginesTestData, workstationsTestData } from "../utils/testData";
 
 const DataContext = createContext<[IData, Function]>([INITIAL_DATA, () => {}]);
 
 const DataProvider = ({ children }: { children: ReactNode }) => {
   const [state, setState] = useState<IData>(INITIAL_DATA);
 
-  useEffect(() => {
-    console.log(workstationsTestData);
+  const handleFetch = async () => {
+    const enginesEndpoint = `https://ubisense-takehome-api.netlify.app/.netlify/functions/engines`;
+    const workstationsEndpoint = `https://ubisense-takehome-api.netlify.app/.netlify/functions/workstations`;
+
+    const reqOptions = {
+      headers: new Headers({
+        method: "GET",
+      }),
+    };
+
+    const enginesResponse = await fetch(enginesEndpoint, reqOptions);
+    const engines = await enginesResponse.json();
+
+    const workstationsResponse = await fetch(workstationsEndpoint, reqOptions);
+    const workstations = await workstationsResponse.json();
 
     setState({
-      engines: enginesTestData,
-      workstations: workstationsTestData,
+      ...state,
+      engines,
+      workstations,
     });
+  };
+
+  useEffect(() => {
+    handleFetch();
   }, []);
 
   return (
